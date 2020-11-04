5ebd2f3d2f21e9e2df52a1745297acb7635a7f35
Ramesh Paudel <rpaudel58@gmail.com>
2020-11-04 22:12:03 +0545

    Project Initialized, Design added

---
+++ GPL_Component1/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GPL_Component1
+{
+    public partial class Form1 : Form
+    {
+      
+    
+        public Form1()
+        {
+            InitializeComponent();
+           
+
+        }
+
+    
+    
+       
+
+
+        void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+           
+            
+        }
+
+             
+     
+
+
+        void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+          
+        }
+
+        //Generate line on canvas
+        private void DrawLineInCanvas(Point currentPoint)
+        {
+          
+        }
+
+ 
+
+
+       
+    
+
+        private void runButton_Click(object sender, EventArgs e)
+        {
+          
+
+
+        }
+
+        private void resetBtn_Click(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void colorPickBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        
+
+        private void penButton_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void circleButtonSelect_Click(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void rectangleBtnSelect_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void squareBtnSelect_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void triangleBtnSelect_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+          
+        }
+
+        private void penTrackBar_ValueChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+         
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
