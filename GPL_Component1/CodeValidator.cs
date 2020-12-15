197f93d545be144988504f8ba812dde535a016ea
Ramesh Paudel <rpaudel58@gmail.com>
2020-12-15 13:00:48 +0545

    Added code validator class to check for valid codes

---
+++ GPL_Component1/CodeValidator.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GPL_Component1
+{
+    class CodeValidator
+    {
+        public int counter = 0;
+        public int LoopCounter = 0;
+        public int lineNumber = 0;
+
+        public Boolean hasLoop = false;
+        public Boolean hasEndLoop = false;
+        public Boolean hasIf = false;
+        public Boolean hasEndif = false;
+        public Boolean isValidCommand = true;
+
+        public Boolean isSomethingInvalid = false;
+
+        public int loopLineNo = 0, endLoopLineNo = 0, ifLineNo = 0, endIfLineNo = 0;
+
+
+        public Boolean checkLoopAndIfValidation(TextBox txtCommand)
+        {
+            int numberOfLines = txtCommand.Lines.Length;
+
+
+            for (int i = 0; i < numberOfLines; i++)
+            {
+                String oneLineCommand = txtCommand.Lines[i];
+                oneLineCommand = oneLineCommand.Trim();
+                if (!oneLineCommand.Equals(""))
+                {
+                    hasLoop = Regex.IsMatch(oneLineCommand.ToLower(), @"\bloop\b");
+                    if (hasLoop)
+                    {
+                        loopLineNo = (i + 1);
+                    }
+                    hasEndLoop = oneLineCommand.ToLower().Contains("endloop");
+                    if (hasEndLoop)
+                    {
+                        endLoopLineNo = (i + 1);
+                    }
+                    hasIf = Regex.IsMatch(oneLineCommand.ToLower(), @"\bif\b");
+                    if (hasIf)
+                    {
+                        ifLineNo = (i + 1);
+                    }
+                    hasEndif = oneLineCommand.ToLower().Contains("endif");
+                    if (hasEndif)
+                    {
+                        endIfLineNo = (i + 1);
+                    }
+                }
+            }
+            if (loopLineNo > 0)
+            {
+                hasLoop = true;
+            }
+            if (endLoopLineNo > 0)
+            {
+                hasEndLoop = true;
+            }
+            if (ifLineNo > 0)
+            {
+                hasIf = true;
+            }
+            if (endIfLineNo > 0)
+            {
+                hasEndif = true;
+            }
+            if (hasLoop)
+            {
+                if (hasEndLoop)
+                {
+                    if (loopLineNo < endLoopLineNo)
+                    {
+
+                    }
+                    else
+                    {
+                        isValidCommand = false;
+                        MessageBox.Show("'ENDLOOP' must be after loop start");
+                    }
+                }
+                else
+                {
+                    isValidCommand = false;
+                    MessageBox.Show("Loop Not Ended with 'ENDLOOP'");
+                }
+            }
+            if (hasIf)
+            {
+                if (hasEndif)
+                {
+                    if (ifLineNo < endIfLineNo)
+                    {
+
+                    }
+                    else
+                    {
+                        isValidCommand = false;
+                        MessageBox.Show("'ENDIF' must be after IF");
+                    }
+                }
+                else
+                {
+                    isValidCommand = false;
+                    MessageBox.Show("IF Not Ended with 'ENDIF'");
+                }
+            }
+            return isValidCommand;
+        }
+
+        public int[] loopPosition()
+        {
+            int[] i = { loopLineNo -1 , endLoopLineNo -1 };
+            return i;
+        }
+
+        public int getLoopCounter()
+        {
+            return LoopCounter;
+        }
+
+    }
+}
