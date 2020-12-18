975e16e2222dde618a551387e841a0f43a45abd9
Ramesh Paudel <rpaudel58@gmail.com>
2020-12-18 19:34:15 +0545

    Refactored Unit tests to their own component

---
+++ GPL_Tests/CommandValidatorTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GPL_Component1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GPL_Component1.Tests
+{
+    [TestClass()]
+    public class CommandValidatorTests
+    {
+        [TestMethod()]
+        public void validateTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void IsDigitsOnlyTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void isValidVarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void checkValidShapeTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void checkValidFunctionTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
