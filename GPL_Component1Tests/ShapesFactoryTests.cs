341311c46a2a1734d2f05d356fe4227dcf205e8e
Ramesh Paudel <rpaudel58@gmail.com>
2020-12-18 19:33:52 +0545

    Refactored tests

---
+++ GPL_Component1Tests/ShapesFactoryTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GPL_Component1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GPL_Component1.Tests
+{
+    [TestClass()]
+    public class ShapesFactoryTests
+    {
+        private CommandValidator sv;
+
+
+        [TestInitialize]
+        public void Initialize()
+        {
+
+            this.sv = new CommandValidator();
+        }
+
+        [TestMethod]
+        public void TestValidCirclePasses()
+        {
+
+            bool isCircle = this.sv.validate("circle");
+            Assert.AreEqual(isCircle, true);
+
+        }
+        [TestMethod]
+        public void TestInvalidShapeFails()
+        {
+
+            bool invalid = this.sv.validate("space");
+            Assert.AreEqual(invalid, false);
+        }
+    }
+}
