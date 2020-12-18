1a42323feec99cd739c0ce4d97d7855eb968bb72
Ramesh Paudel <rpaudel58@gmail.com>
2020-12-18 19:32:38 +0545

    Added condition parser test to validate all the conditions in the source code

---
+++ GPL_Component1Tests/ConditionParserTests.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GPL_Component1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GPL_Component1.Tests
+{
+    [TestClass()]
+    public class ConditionParserTests
+    {
+
+
+        private ConditionParser cp;
+
+
+        [TestInitialize]
+        public void Initialize()
+        {
+
+            VariableParser.GetInstance();
+            this.cp = new ConditionParser();
+        }
+
+       
+        [TestMethod()]
+        public void checkValidPassingIfConditionTest()
+        {
+            VariableParser.setVariable("count = 10");
+            string validCondition = "if count < 20 then";
+            this.cp.setCondition(validCondition);
+            Assert.IsTrue(this.cp.validateExpression());
+        }
+
+
+        [TestMethod()]
+        public void checkInvalidFailingIfConditionTest()
+        {
+            VariableParser.setVariable("fail = 10");
+            string validCondition = "if fail > 20 then";
+            this.cp.setCondition(validCondition);
+            Assert.IsFalse(this.cp.validateExpression());
+        }
+
+        [TestMethod()]
+        public void checkValidPassingLoopConditionTest()
+        {
+            VariableParser.setVariable("pass = 10");
+            string validLoopCondition = "loop pass < 20";
+            this.cp.setLoopCondition(validLoopCondition);
+            Assert.IsTrue(this.cp.validateExpression());
+        }
+
+        [TestMethod()]
+        public void checkValidFailingLoopConditionTest()
+        {
+            VariableParser.setVariable("test = 10");
+            string validLoopCondition = "loop test > 20 then";
+            this.cp.setLoopCondition(validLoopCondition);
+            Assert.IsFalse(this.cp.validateExpression());
+        }
+
+
+    }
+}
